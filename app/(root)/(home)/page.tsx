eb5c78a26a3ca25c28982ef9d46a060f7fce776b
alok_mishra14 <143alokmishra143@gmail.com>
2023-12-28 21:26:12 +0530

    solve u+219 sowing

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -103,7 +103,7 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There #U+2019 s no question to show"
+            title="There`s no question to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
