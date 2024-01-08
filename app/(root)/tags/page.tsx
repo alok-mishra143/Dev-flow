546a9844587e19fc2ec1d9fab27b337ab560c1c0
alok_mishra14 <143alokmishra143@gmail.com>
2024-01-08 22:45:06 +0530

    adding the Tag page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+
+import { TagFilters, UserFilters } from "@/constants/filters";
+
+import type { SearchParamsProps } from "@/types";
+import type { Metadata } from "next";
+import LocalSearchBar from "@/components/shared/Search/LocalSearchBar";
+import { getAllTags } from "@/lib/actions/tag.action";
+
+export const metadata: Metadata = {
+  title: "Tags — DevOverflow",
+};
+
+const Page = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllTags({
+    // searchQuery: searchParams.q,
+    // filter: searchParams.filter,
+    // page: searchParams.page ? +searchParams.page : 1,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">All Tags</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/community"
+          placeholder="Search for amazing minds"
+          iconPostition="left"
+          imgsrc="/assets/icons/search.svg"
+          otherClass="flex-1"
+        />
+
+        <Filter
+          filters={UserFilters}
+          otherClass="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.tags.length > 0 ? (
+          result.tags.map((tag: any) => (
+            <Link
+              href={`/tags/${tag._id}`}
+              key={tag._id}
+              className="shadow-light100_darknone"
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+
+                {tag.description && (
+                  <p className="small-regular text-dark500_light700 mt-4">
+                    {tag.description}
+                  </p>
+                )}
+
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.questions.length}+
+                  </span>{" "}
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoResult
+            title="No Tags Found"
+            description="It appears that there are not tags found at the moment 😔. Ask a Question and kickstart the
+            discussion with tags. our query could be the next big thing others learn from. Get
+            involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </section>
+
+      {/* <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div> */}
+    </>
+  );
+};
+
+export default Page;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,19 +1,24 @@
-"use server"
+"use server";
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../moongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import {
+  GetAllTagsParams,
+  GetTagByIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
+import Tag from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
 
-export async function getTopIneractedTags(prams:GetTopInteractedTagsParams){
-
+export async function getTopIneractedTags(prams: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
 
-    const {userId,limit=3}=prams;
+    const { userId, limit = 3 } = prams;
 
-    const user =await User.findById(userId);
+    const user = await User.findById(userId);
 
-    if(!user){
+    if (!user) {
       throw new Error("user not found");
     }
 
@@ -21,16 +26,83 @@ export async function getTopIneractedTags(prams:GetTopInteractedTagsParams){
 
     //interaction
 
-
-    return [{_id:1, name:'tag1'} ,{_id:2, name:'tag2'} ,{_id:3, name:'tag3'}]
-
-
-
-    
+    return [
+      { _id: 1, name: "tag1" },
+      { _id: 2, name: "tag2" },
+      { _id: 3, name: "tag3" },
+    ];
   } catch (error) {
     console.log(error);
     throw error;
-    
   }
+}
 
+export async function getTagById(params: GetTagByIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId } = params;
+
+    const tag = await Tag.findOne({
+      _id: tagId,
+    });
+
+    return tag;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+//get all getTags
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    //! const { page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    // Calculate the number of tags to skip based on the page number and page size
+
+    //!  const skipAmount = (page - 1) * pageSize;
+
+    const query: FilterQuery<typeof Tag> = {};
+
+    // if (searchQuery) {
+    //   query.$or = [{ name: { $regex: new RegExp(searchQuery, "i") } }];
+    // }
+
+    // let sortOptions = {};
+
+    // switch (filter) {
+    //   case "popular":
+    //     sortOptions = { questions: -1 };
+    //     break;
+    //   case "recent":
+    //     sortOptions = { createdAt: -1 };
+    //     break;
+    //   case "name":
+    //     sortOptions = { name: 1 };
+    //     break;
+    //   case "old":
+    //     sortOptions = { createdAt: 1 };
+    //     break;
+    //   default:
+    //     break;
+    // }
+
+    const tags = await Tag.find({});
+    // .sort(sortOptions)
+    // .skip(skipAmount)
+    // .limit(pageSize);
+
+    //const totalTags = await Tag.countDocuments(query);
+
+    //const isNext = totalTags > skipAmount + tags.length;
+
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
