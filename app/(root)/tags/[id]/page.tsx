c6147d1d6a4a96fc1799b873ad95e4f0bd3c1c77
alok_mishra14 <143alokmishra143@gmail.com>
2024-01-09 15:08:08 +0530

    implementation of tag details

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,67 @@
+import QuestionCard from "@/components/card/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/Search/LocalSearchBar";
+import { getQuestionByTagId } from "@/lib/actions/tag.action";
+import { URLProps } from "@/types";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  const { userId: clerkId } = auth();
+
+  const result = await getQuestionByTagId({
+    tagId: params.id,
+    searchQuery: searchParams.q,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+      <div className="mt-11 w-full">
+        <LocalSearchBar
+          route={`/tags/${params.id}`}
+          iconPostition="left"
+          imgsrc="/assets/icons/search.svg"
+          placeholder="Search tag questions"
+          otherClass="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              clerkId={clerkId}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="No Tag Questions Found"
+            description="It appears that there are no saved questions in your collection at the moment 😔.Start exploring and saving questions that pique your interest 🌟"
+            link="/"
+            linkTitle="Explore Questions"
+          />
+        )}
+      </div>
+
+      {/* <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div> */}
+    </>
+  );
+};
+
+export default Page;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -4,11 +4,13 @@ import User from "@/database/user.model";
 import { connectToDatabase } from "../moongoose";
 import {
   GetAllTagsParams,
+  GetQuestionByTagIdParams,
   GetTagByIdParams,
   GetTopInteractedTagsParams,
 } from "./shared.types";
 import Tag from "@/database/tag.model";
 import { FilterQuery } from "mongoose";
+import Question from "@/database/question.model";
 
 export async function getTopIneractedTags(prams: GetTopInteractedTagsParams) {
   try {
@@ -101,6 +103,50 @@ export async function getAllTags(params: GetAllTagsParams) {
     //const isNext = totalTags > skipAmount + tags.length;
 
     return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionByTagId(params: GetQuestionByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    // Calculate the number of questions to skip based on the page number and page size
+
+    const skipAmount = (page - 1) * pageSize;
+
+    const tagFilter: FilterQuery<typeof Tag> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+        skip: skipAmount,
+        limit: pageSize + 1, // +1 to check if there is next page
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("Tag not found");
+    }
+
+    const questions = tag.questions;
+
+    const isNext = tag.questions.length > pageSize;
+
+    return { tagTitle: tag.name, questions, isNext };
   } catch (error) {
     console.log(error);
     throw error;
